f2795495c19c1cb6395c1c40b176f8e5a927993a
Bojan B <83359157+bonjadev@users.noreply.github.com>
2023-04-29 06:54:40 +0200

    First commit.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Static_and_Private_constructor
+{
+    class PrConstructor
+    {
+        public int a;
+        public PrConstructor(int b)
+        {
+            this.a = b;
+        }
+        private PrConstructor()
+        {
+            Console.WriteLine("Private Constructor");
+        }
+
+    }
+
+    class test
+    {
+        static void Main()
+        {
+            PrConstructor obj = new PrConstructor(5);
+            Console.WriteLine(obj.a);
+            Console.ReadLine();
+        }
+    }
+}
